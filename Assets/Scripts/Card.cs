3292519719cebea5823d63adb032d3d3828f57cb
Colin Buckley <cbuckles17@no.com>
2019-09-08 21:46:57 -0500

    Cleaning up Card and CardStack
    Just cleaning up the code for Card and CardStack. Added a bunch
    of commenting. Also added some error checking.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
+// Card contains all card faces and back. A card object can
+// be any card in the deck.
 public class Card : MonoBehaviour
 {
     private SpriteRenderer _CardSR;
@@ -10,42 +11,48 @@ public class Card : MonoBehaviour
     public List<Sprite> Faces;
     public Sprite Back;
     public int FaceIndex;
-    public bool ShowFace;
 
+    // Awake is used to initialize any variables or game 
+    // state before the game starts.
     private void Awake()
     {
         _CardSR = this.GetComponent<SpriteRenderer>();
     }
 
+    // Rank will return the rank of the card based on the
+    // face that is currently set. Number cards return their
+    // value. Face cards return 10. Aces return 11.
     public int Rank()
     {
-        //4 cards per rank
-        //+2 for starting rank at 2
+        // 4 cards per rank
+        // +2 for starting rank at 2
         int rank = (FaceIndex / 4) + 2;
         
-        //if it is a face card (minus aces) then value is 10
+        // if it is a face card (minus aces) then value is 10
         if(rank > 11)
         {
             return 10;
         }
-        //pass back rank for all others
+        // pass back rank for all others
         else
         {
             return rank;
         }
     }
 
+    // ToogleFace will set the sprite for the card. Based on
+    // boolean value it will show the face or the back.
     public void ToggleFace(bool showFace)
     {
-        if(showFace)
+        // show a face
+        if (showFace)
         {
             _CardSR.sprite = Faces[FaceIndex];
         }
+        // show the back
         else
         {
             _CardSR.sprite = Back;
         }
-
-        ShowFace = showFace;
     }
 }
--- Assets/Scripts/CardStack.cs
+++ Assets/Scripts/CardStack.cs
@@ -1,33 +1,120 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class CardStack : MonoBehaviour {
-    private List<GameObject> Cards;
+// CardStack is a collection of Card objects and operations to
+// support interactions with it.
+public class CardStack : MonoBehaviour
+{
+    private static System.Random _Rand = new System.Random();
+    private List<GameObject> _Cards;
+    
     public GameObject CardPrefab;
     public float cardSpacing;
 
-    private static System.Random Rand = new System.Random();
-
+    // Awake is used to initialize any variables or game 
+    // state before the game starts.
     private void Awake()
     {
-        Cards = new List<GameObject>();
+        _Cards = new List<GameObject>();
     }
 
+    // GetCard will get a card and return it. This is needed since
+    // Cards is a private object (we don't want anyone messing with
+    // that).
+    public GameObject GetCard(int index)
+    {
+        // check the cards list is not null and that the index exists
+        if (_Cards != null && _Cards.Count > index)
+        {
+            return _Cards[index];
+        }
+        // empty cards list or bad index
+        else
+        {
+            Debug.Log("No Card in the Card Stack at index: " + index);
+            return null;
+        }
+    }
+
+    // AddCard is basically a stack push function. It pushes to the
+    // tail of the card stack. It will set itself as the parent of
+    // the card.
+    public void AddCard(GameObject cardGO, bool showFace)
+    {
+        // setting up the x position with the given card spacing and 
+        // card count
+        float xPosition = (_Cards.Count + 1f) * cardSpacing;
+
+        // move the card to a new parent card stack
+        cardGO.transform.SetParent(this.transform);
+        cardGO.transform.localPosition = new Vector3(xPosition, 0f, 0f);
+
+        Card card = cardGO.GetComponent<Card>();
+        // set the card face up or down based on the given boolean
+        card.ToggleFace(showFace);
+
+        SpriteRenderer cardSR = cardGO.GetComponent<SpriteRenderer>();
+        // set the sorting order so cards aren't displayed out of order
+        cardSR.sortingOrder = _Cards.Count;
+
+        _Cards.Add(cardGO);
+    }
+
+    // RemoveCard is basically a stack pop function. It pops the tail
+    // of the card stack and then returns it. This does not destroy 
+    // the card object as we will move the card to another card stack 
+    // and that card stack will become the parent of the object.
+    public GameObject RemoveCard()
+    {
+        // make sure there are cards 
+        if (_Cards.Count > 0)
+        {
+            GameObject card = _Cards[_Cards.Count - 1];
+            _Cards.RemoveAt(_Cards.Count - 1);
+
+            return card;
+        }
+        // no cards
+        else
+        {
+            Debug.Log("No Cards left in Card Stack to Remove");
+            return null;
+        }
+    }
+
+    // Clear is for cleaning up a card stack. It will loop through 
+    // all of its cards and destroy them.
+    public void Clear()
+    {
+        // loop through all of cards
+        foreach (GameObject cardGO in _Cards)
+        {
+            Destroy(cardGO);
+        }
+
+        _Cards.Clear();
+    }
+
+    // Value gets the rank of all the cards in the card stack.
+    // Aces is a special case where it can have rank 1 or 11.
+    // So if using 11 knocks the value over 21 then use 1 for 
+    // its rank.
     public int Value()
     {
         int value = 0;
 
-        foreach (GameObject cardGO in Cards)
+        // loop through all the cards
+        foreach (GameObject cardGO in _Cards)
         {
             int rank = cardGO.GetComponent<Card>().Rank();
 
-            //if aces and using 11 as rank brings value over 21
+            // if aces and using 11 as rank brings value over 21
             if (rank == 11 && (value + rank) > 21)
             {
                 value += 1;
             }
+            // else use given rank
             else
             {
                 value += rank;
@@ -37,89 +124,56 @@ public class CardStack : MonoBehaviour {
         return value;
     }
 
+    // MakeDeck will make the deck for the game. It shuffles the deck
+    // based on the Shuffle function.
     public void MakeDeck()
     {
         int numFaces = CardPrefab.GetComponent<Card>().Faces.Count;
-
+        // get the shuffled positions for the deck
         List<int> shuffledPositions = Shuffle(numFaces);
 
-        for (int i = 0; i < numFaces; i++)
+        // loop through all the faces
+        for (int cardIndex = 0; cardIndex < numFaces; cardIndex++)
         {
+            // instantiate the card
             GameObject cardGO = (GameObject)Instantiate(CardPrefab);
-            float co = (i + 1f) * cardSpacing;
+            // setting up the x position with the given card spacing
+            float xPosition = (cardIndex + 1f) * cardSpacing;
 
             cardGO.transform.SetParent(this.transform, false);
-            cardGO.transform.localPosition = new Vector3(co, 0f, 0f);
+            cardGO.transform.localPosition = new Vector3(xPosition, 0f, 0f);
 
             Card card = cardGO.GetComponent<Card>();
-            card.FaceIndex = shuffledPositions[i];
+            // set the face to the shuffled index
+            card.FaceIndex = shuffledPositions[cardIndex];
+            // set the card face down since it is the deck
             card.ToggleFace(false);
 
             SpriteRenderer cardSR = cardGO.GetComponent<SpriteRenderer>();
-            cardSR.sortingOrder = i;
-      
-            Cards.Add(cardGO);
+            // set the sorting order so cards aren't displayed out of order
+            cardSR.sortingOrder = cardIndex;
+
+            _Cards.Add(cardGO);
         }
     }
 
+    // Shuffle will create a list of ints that is shuffled based on
+    // Fisher–Yates algorithm.
     public static List<int> Shuffle(int count)
     {
+        // create a list of ints starting at 0 and iterating count amount 
+        // of times
         List<int> shuffledPositions = Enumerable.Range(0, count).ToList();
 
+        // execute Fisher–Yates algorithm
         for (int position = shuffledPositions.Count - 1; position > 0; position--)
         {
-            int randPosition = Rand.Next(position + 1);
+            int randPosition = _Rand.Next(position + 1);
             var temp = shuffledPositions[position];
             shuffledPositions[position] = shuffledPositions[randPosition];
             shuffledPositions[randPosition] = temp;
         }
 
         return shuffledPositions;
-    }
-
-    public GameObject GetCard(int index)
-    {
-        if(Cards[index] == null)
-        {
-            return null;
-        }
-        else
-        {
-            return Cards[index];
-        }
-    }
-
-    public void AddCard(GameObject cardGO, bool showFace)
-    {
-        float co = (Cards.Count + 1f) * cardSpacing;
-
-        cardGO.transform.SetParent(this.transform);
-        cardGO.transform.localPosition = new Vector3(co, 0f, 0f);
-
-        Card card = cardGO.GetComponent<Card>();
-        card.ToggleFace(showFace);
-
-        SpriteRenderer cardSR = cardGO.GetComponent<SpriteRenderer>();
-        cardSR.sortingOrder = Cards.Count;
-
-        Cards.Add(cardGO);
-    }
-
-    public GameObject RemoveCard()
-    {
-        GameObject card = Cards[Cards.Count - 1];
-        Cards.RemoveAt(Cards.Count - 1);
-
-        return card;
-    }
-
-    public void Clear()
-    {
-        foreach (GameObject cardGO in Cards)
-        {
-            Destroy(cardGO);
-        }
-
-        Cards.Clear();
     }
 }
